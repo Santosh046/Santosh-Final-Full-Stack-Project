f33ad0bbfec7a1e4b792d996e2e94872fb133fd8
Santosh046 <155556289+Santosh046@users.noreply.github.com>
2024-04-02 08:26:08 +0530

    Add files via upload

---
+++ Employee-mgmt-systm/src/test/java/com/santoshsEmployee/EmployeeMgmtSystmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santoshsEmployee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeMgmtSystmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
